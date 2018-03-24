1b96f155926cf03d15116e9128d443850d4fc663
Shawn Toffel <shawn.toffel@gmail.com>
2018-03-24 17:22:09 -0600

    Add candidate sort by rank

--- candidate.go
+++ candidate.go
@@ -6,3 +6,17 @@ type Candidate struct {
 	Name string
 	Rank int
 }
+
+type ByRank Candidates
+
+func (c ByRank) Len() int {
+	return len(c)
+}
+
+func (c ByRank) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c ByRank) Less(i, j int) bool {
+	return c[i].Rank < c[j].Rank
+}
