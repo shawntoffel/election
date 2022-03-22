f1a0adfdb26babcab1ca0f675c42f5a5fbc6b0a9
Shawn Toffel <shawn.toffel@gmail.com>
2022-03-22 04:51:32 +0000

    Implement round summaries

--- result.go
+++ result.go
@@ -1,7 +1,11 @@
 package election
 
 type Result struct {
-	Candidates Candidates `json:"candidates"`
-	Events     Events     `json:"events"`
-	Summaries  Events     `json:"summaries"`
+	Title      string     `json:"title,omitempty" yaml:"title,omitempty"`
+	NumSeats   int        `json:"numSeats,omitempty" yaml:"numSeats,omitempty"`
+	NumBallots int        `json:"numBallots,omitempty" yaml:"numBallots,omitempty"`
+	Precision  int        `json:"precision,omitempty" yaml:"precision,omitempty"`
+	Seed       int64      `json:"seed,omitempty" yaml:"seed,omitempty"`
+	Elected    Candidates `json:"elected,omitempty" yaml:"elected,omitempty"`
+	Summary    *Summary   `json:"summary,omitempty" yaml:"summary,omitempty"`
 }
